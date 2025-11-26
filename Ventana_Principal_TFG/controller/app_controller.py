1ef9f96b2ff3b82f2c8bda65d395bec73f74a238
AdriTg00 <adrian2000tg@gmail.com>
2025-11-26 11:52:34 +0100

    Add local user check and improve launcher flow
    Introduced comprobar_usuario_local to check for an existing local user in usuario_local.json and launch the appropriate window automatically. Refactored app_controller.py to streamline the user flow and updated related logic. Added configuracion.db and updated usuario_local.json with a new user.

--- Ventana_Principal_TFG/controller/app_controller.py
+++ Ventana_Principal_TFG/controller/app_controller.py
@@ -4,6 +4,8 @@ from .cargarPartidas import cargar
 from .configuracion import configuracion
 from .introduccionNombre import introducirNombre
 from PySide6.QtCore import Qt
+import os
+import json
 
 
 class AppController:
@@ -21,31 +23,45 @@ class AppController:
         self.launcher.idioma_cambiado.connect(self.config_window.apply_language)
         self.launcher.idioma_cambiado.connect(self.carg_partidas.apply_language)
         self.launcher.idioma_cambiado.connect(self.introducir_nombre.apply_language)
+        self.comprobar_usuario_local()
     
 
     def mostrar_launcher(self, user_id):
         print("Usuario validado:", user_id)
         self.user_id = user_id   # Guardar ID en memoria
         self.introducir_nombre.hide()
-        self.launcher.show()
+     
         
     def mostrar_partidas_guardadas(self):
         print("Abriendo ventana de configuración desde el controlador")
         self.carg_partidas.setWindowModality(Qt.ApplicationModal) 
         self.carg_partidas.show()  
-    
-    def abrir_nueva_partida(self):
-        print("Abriendo ventana de configuración desde el controlador")
-        self.introducir_nombre.setWindowModality(Qt.ApplicationModal) 
-        self.introducir_nombre.show()  
         
 
     def mostrar_configuracion(self):
         print("Abriendo ventana de cargar partida desde el controlador")
         self.config_window.setWindowModality(Qt.ApplicationModal) 
         self.config_window.show()  
-        
     
+    
+    def abrir_nueva_partida(self):
+        print("Abriendo ventana de introducción de nombre desde el controlador")
+        self.introducir_nombre.setWindowModality(Qt.ApplicationModal)
+        self.introducir_nombre.show()
+    
+    def comprobar_usuario_local(self):
+        if os.path.exists("usuario_local.json"):
+            with open("usuario_local.json", "r") as f:
+                datos = json.load(f)
+
+            print("Usuario ya registrado → entrando directo al launcher")
+            self.mostrar_launcher(datos["id"])
+            self.launcher.show()
+        else:
+            print("No existe usuario guardado → pedir nombre")
+            self.introducir_nombre.setWindowModality(Qt.ApplicationModal) 
+            self.introducir_nombre.show()  
+
 
    
    
