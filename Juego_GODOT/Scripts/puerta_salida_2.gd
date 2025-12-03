4f95654718fb657671ed9c9925cc978aa60ebdeb
AdriTg00 <adrian2000tg@gmail.com>
2025-12-03 11:01:52 +0100

    Refactor and reorganize puerta_salida scripts
    Moved puerta_salida.gd and its .uid file to the Scripts directory and added puerta_salida_2.gd with its .uid. Updated scene files (escenario3.tscn, puerta_salida.tscn, segundo_nivel.tscn, tercer_nivel.tscn) to reference the new script locations, supporting better project organization.

---
+++ Juego_GODOT/Scripts/puerta_salida_2.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var anim = $StaticBody2D/AnimatedSprite2D
+
+signal jugador_entro_puerta
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Rey":
+		print('Jugador entro a la puerta')  # cambia si tu jugador se llama distinto
+		entrar_nivel(body)
+
+func entrar_nivel(jugador):
+	anim.play("open")
+		# Bloqueamos al jugador completamente
+	jugador.bloqueado = true
+	jugador.velocity = Vector2.ZERO
+	jugador.set_process_input(false)
+	await anim.animation_finished
+
+	
+
+
+	var animacion_jugador = jugador.get_node("AnimatedSprite2D") 
+	animacion_jugador.play("door_in")
+	await animacion_jugador.animation_finished
+
+	emit_signal("jugador_entro_puerta")
