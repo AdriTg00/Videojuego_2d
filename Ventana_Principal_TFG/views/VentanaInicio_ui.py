8b81ae70e9858bdc5c61a2729a9b08f2fe2d998a
AdriTg00 <adrian2000tg@gmail.com>
2025-12-06 17:45:21 +0100

    Fix import path for resources_rc in VentanaInicio_ui.py
    Updated the import statement in VentanaInicio_ui.py to use the correct module path for resources_rc. This resolves issues with resource loading due to incorrect import location.

--- Ventana_Principal_TFG/views/VentanaInicio_ui.py
+++ Ventana_Principal_TFG/views/VentanaInicio_ui.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QMainWindow,
     QPushButton, QSizePolicy, QStatusBar, QWidget)
-import resources_rc
+import resources.resources_rc
 
 class Ui_launcher(object):
     def setupUi(self, launcher):
